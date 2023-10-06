dffcda41629db821784d0f940738538fd1001b2e
Sindhuri Lanka <gloryratna94@gmail.com>
2023-10-06 18:54:11 +0530

    login register

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './../logo.svg';
 import './App.css';
+import { Button, Stack } from 'react-bootstrap';
 
 function App() {
   return (
@@ -19,6 +20,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <Stack direction="horizontal" gap={2}>
+        <Button as="a" variant="primary">
+          Button as link
+        </Button>
+        <Button as="a" variant="success">
+          Button as link
+        </Button>
+      </Stack>;
     </div>
   );
 }
