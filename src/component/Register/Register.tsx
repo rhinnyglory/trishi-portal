b275b97aaa7b1db97e57729f543987284cdc4b6e
Sindhuri Lanka <gloryratna94@gmail.com>
2023-10-07 22:43:21 +0530

    adding validations

--- src/component/Register/Register.tsx
+++ src/component/Register/Register.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { useForm, Resolver } from "react-hook-form";
 import './Register.css';
 
+type Inputs = {
+    firstName: string
+    lastName: string
+}
+
+const resolver: Resolver<Inputs> = async (values) => {
+    return {
+        values: values.firstName ? values : {},
+        errors: !values.firstName ? {
+            firstName: {
+                type: "required",
+                message: "This is required.",
+            },
+        } : {},
+    }
+}
+
 const Register = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data: any) => console.log(data);
+    console.log(errors['First name']?.type);
+
+
     return (
         <div className="container register registration-block">
             <div className="row">
@@ -23,58 +46,58 @@ const Register = () => {
                     <div className="tab-content" id="myTabContent">
                         <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
                             <h3 className="register-heading">Trishi Portal</h3>
-                            <form>
+                            <form onSubmit={handleSubmit(onSubmit)}>
                                 <div className="row register-form">
                                     <div className="col-md-6">
                                         <div className=" form-group mb-3">
-                                            <input type="text" className="form-control" placeholder="First Name *" value="" />
+                                            <input type="text" className="form-control" placeholder="First name" {...register("First name", { required: true, maxLength: 80 })} />
                                         </div>
                                         <div className=" form-group mb-3">
-                                            <input type="text" className="form-control" placeholder="Middle Name *" value="" />
+                                            <input type="text" className="form-control" placeholder="Middle Name *" />
                                         </div>
                                         <div className=" form-group mb-3">
-                                            <input type="text" className="form-control" placeholder="Last Name *" value="" />
+                                            <input type="text" className="form-control" placeholder="Last name" {...register("Last name", { required: true, maxLength: 100 })} />
                                         </div>
                                         <div className=" form-group mb-3">
-                                            <input type="email" className="form-control" placeholder="Your Email *" value="" />
+                                            <input type="text" className="form-control" placeholder="Email" {...register("Email", { required: true, pattern: /^\S+@\S+$/i })} />
                                         </div>
                                         <div className=" form-group mb-3">
-                                            <input type="text" minLength={10} maxLength={10} name="txtEmpPhone" className="form-control" placeholder="Your Phone *" value="" />
+                                            <input type="tel" className="form-control" placeholder="Mobile number" {...register("Mobile number", { required: true, minLength: 6, maxLength: 12 })} />
                                         </div>
                                         <div className=" form-group mb-3">
-                                            <input type="text" className="form-control" placeholder="Address Line 1 *" value="" />
+                                            <input type="text" className="form-control" placeholder="Address Line 1 *" />
                                         </div>
                                         <div className=" form-group mb-3">
-                                            <input type="text" className="form-control" placeholder="Address Line 2 *" value="" />
+                                            <input type="text" className="form-control" placeholder="Address Line 2 *" />
                                         </div>
                                         <div className=" form-group mb-3">
-                                            <input type="text" className="form-control" placeholder="Address Line 3 *" value="" />
+                                            <input type="text" className="form-control" placeholder="Address Line 3 *" />
                                         </div>
                                     </div>
                                     <div className="col-md-6">
                                         <div className=" form-group mb-3">
-                                            <input type="text" className="form-control" placeholder="Company Name *" value="" />
+                                            <input type="text" className="form-control" placeholder="Company Name *" />
                                         </div>
                                         <div className=" form-group mb-3">
-                                            <input type="text" className="form-control" placeholder="GSTIN *" value="" />
+                                            <input type="text" className="form-control" placeholder="GSTIN *" />
                                         </div>
                                         <div className=" form-group mb-3">
-                                            <input type="text" className="form-control" placeholder="PAN *" value="" />
+                                            <input type="text" className="form-control" placeholder="PAN *" />
                                         </div>
                                         <div className=" form-group mb-3">
-                                            <input type="text" className="form-control" placeholder="PIN Code *" value="" />
+                                            <input type="text" className="form-control" placeholder="PIN Code *" />
                                         </div>
                                         <div className=" form-group mb-3">
-                                            <input type="text" className="form-control" placeholder="Location *" value="" />
+                                            <input type="text" className="form-control" placeholder="Location *" />
                                         </div>
                                         <div className=" form-group mb-3">
-                                            <input type="text" className="form-control" placeholder="Username *" value="" />
+                                            <input type="text" className="form-control" placeholder="Username *" />
                                         </div>
                                         <div className=" form-group mb-3">
-                                            <input type="password" className="form-control" placeholder="Password *" value="" />
+                                            <input type="password" className="form-control" placeholder="Password *" />
                                         </div>
                                         <div className=" form-group mb-3">
-                                            <input type="password" className="form-control" placeholder="Confirm Password *" value="" />
+                                            <input type="password" className="form-control" placeholder="Confirm Password *" />
                                         </div>
                                         <input type="submit" className="btnRegister" value="Register" />
                                     </div>
@@ -86,24 +109,24 @@ const Register = () => {
                             <div className="row register-form">
                                 <div className="col-md-6">
                                     <div className=" form-group mb-3">
-                                        <input type="text" className="form-control" placeholder="First Name *" value="" />
+                                        <input type="text" className="form-control" placeholder="First Name *" />
                                     </div>
                                     <div className=" form-group mb-3">
-                                        <input type="text" className="form-control" placeholder="Last Name *" value="" />
+                                        <input type="text" className="form-control" placeholder="Last Name *" />
                                     </div>
                                     <div className=" form-group mb-3">
-                                        <input type="email" className="form-control" placeholder="Email *" value="" />
+                                        <input type="email" className="form-control" placeholder="Email *" />
                                     </div>
                                     <div className=" form-group mb-3">
-                                        <input type="text" maxLength={10} minLength={10} className="form-control" placeholder="Phone *" value="" />
+                                        <input type="text" maxLength={10} minLength={10} className="form-control" placeholder="Phone *" />
                                     </div>
                                 </div>
                                 <div className="col-md-6">
                                     <div className=" form-group mb-3">
-                                        <input type="password" className="form-control" placeholder="Password *" value="" />
+                                        <input type="password" className="form-control" placeholder="Password *" />
                                     </div>
                                     <div className=" form-group mb-3">
-                                        <input type="password" className="form-control" placeholder="Confirm Password *" value="" />
+                                        <input type="password" className="form-control" placeholder="Confirm Password *" />
                                     </div>
                                     <div className=" form-group mb-3">
                                         <select className="form-control">
@@ -114,7 +137,7 @@ const Register = () => {
                                         </select>
                                     </div>
                                     <div className=" form-group mb-3">
-                                        <input type="text" className="form-control" placeholder="`Answer *" value="" />
+                                        <input type="text" className="form-control" placeholder="`Answer *" />
                                     </div>
                                     <input type="submit" className="btnRegister" value="Register" />
                                 </div>
