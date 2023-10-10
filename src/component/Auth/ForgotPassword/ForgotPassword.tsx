6e562d538621ad6c54bd3fc2b8e1c2be2da85126
joelrajusomala <joelrajusomala@gmail.com>
2023-10-11 03:09:24 +0530

    added login validation

--- src/component/Auth/ForgotPassword/ForgotPassword.tsx
+++ src/component/Auth/ForgotPassword/ForgotPassword.tsx
@@ -2,67 +2,53 @@ import React from "react"
 import "./ForgotPassword.css"
 
 const ForgotPassword = () => {
-  return (
-    <div className="tab-content" id="myTabContent">
-      <div
-        className="tab-pane fade show active"
-        id="home"
-        role="tabpanel"
-        aria-labelledby="home-tab">
-        <h3 className="login-heading">Forgot Password</h3>
-        <div className="row authenticate-form">
-          <div className="row">
-            <div className="col-md-6 offset-3">
-              <div className=" form-group mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter Email or Mobile No *"
-                  value=""
-                />
-              </div>
-            </div>
-          </div>
+    return (
+        <div className="tab-content" id="myTabContent">
+            <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                <h3 className="login-heading">Forgot Password</h3>
+                <div className="row authenticate-form">
+                    <div className="row">
+                        <div className="col-md-6 offset-3">
+                            <div className=" form-group mb-3">
+                                <input type="text" className="form-control" placeholder="Enter Email or Mobile No *" value="" />
+                            </div>
+                        </div>
+                    </div>
 
-          <div className="row ">
-            <div className="col-md-8">
-              <div className="form-group">
-                <input type="submit" className="btnOtp" value="Send Otp" />
-              </div>
-            </div>
-          </div>
-          <div className="row mt-4">
-            <h6>
-              Didn't get Otp?{" "}
-              <span>
-                <a href="#login">Resend</a>
-              </span>
-            </h6>
-          </div>
-          <div className="row mt-4">
-            <div className="col-md-6 offset-3">
-              <div className=" form-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter Otp *"
-                  value=""
-                />
-              </div>
-            </div>
-          </div>
+                    <div className="row ">
+                        <div className="col-md-8">
+                            <div className="form-group">
+                                <input type="submit" className="btnOtp" value="Send Otp" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row mt-4">
+                        <h6>
+                            Didn't get Otp?{" "}
+                            <span>
+                                <a href="#login">Resend</a>
+                            </span>
+                        </h6>
+                    </div>
+                    <div className="row mt-4">
+                        <div className="col-md-6 offset-3">
+                            <div className=" form-group">
+                                <input type="text" className="form-control" placeholder="Enter Otp *" value="" />
+                            </div>
+                        </div>
+                    </div>
 
-          <div className="row">
-            <div className="col-md-8 mt-3">
-              <div className="form-group">
-                <input type="submit" className="btnOtp" value="Submit" />
-              </div>
+                    <div className="row">
+                        <div className="col-md-8 mt-3">
+                            <div className="form-group">
+                                <input type="submit" className="btnOtp" value="Submit" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default ForgotPassword
--- src/component/Auth/ResetPassword/ResetPassword.tsx
+++ src/component/Auth/ResetPassword/ResetPassword.tsx
@@ -2,48 +2,33 @@ import React from "react"
 import "./ResetPassword.css"
 
 const ResetPassword = () => {
-  return (
-    <div className="tab-content" id="myTabContent">
-      <div
-        className="tab-pane fade show active"
-        id="home"
-        role="tabpanel"
-        aria-labelledby="home-tab"
-      >
-        <h3 className="login-heading">Reset Password</h3>
-        <div className="row authenticate-form">
-          <div className="row">
-            <div className="col-md-6 offset-3">
-              <div className=" form-group mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter new password *"
-                  value=""
-                />
-              </div>
-              <div className=" form-group mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Confirm password *"
-                  value=""
-                />
-              </div>
-            </div>
-          </div>
+    return (
+        <div className="tab-content" id="myTabContent">
+            <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                <h3 className="login-heading">Reset Password</h3>
+                <div className="row authenticate-form">
+                    <div className="row">
+                        <div className="col-md-6 offset-3">
+                            <div className=" form-group mb-3">
+                                <input type="text" className="form-control" placeholder="Enter new password *" value="" />
+                            </div>
+                            <div className=" form-group mb-3">
+                                <input type="text" className="form-control" placeholder="Confirm password *" value="" />
+                            </div>
+                        </div>
+                    </div>
 
-          <div className="row ">
-            <div className="col-md-8">
-              <div className="form-group">
-                <input type="submit" className="btnReset" value="Reset" />
-              </div>
+                    <div className="row ">
+                        <div className="col-md-8">
+                            <div className="form-group">
+                                <input type="submit" className="btnReset" value="Reset" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default ResetPassword
